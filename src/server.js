5d0d9f8c98a4a28e2d0bfd4015d86549e85e352b
apachedragonfly <100011406+apachedragonfly@users.noreply.github.com>
2025-01-09 21:50:19 -0330

    Removed backend

--- src/server.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err)); 
